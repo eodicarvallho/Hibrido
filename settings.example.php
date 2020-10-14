edcb323200be2387adb4c7d1cb88d6d01825554e
Diego Carvalho <diegocarvalho.dev@gmail.com>
2020-10-14 04:20:06 -0300

    estrutura base

---
+++ settings.example.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('DB_HOST') or define('DB_HOST', '');
+defined('DB_USER') or define('DB_USER', '');
+defined('DB_NAME') or define('DB_NAME', '');
+defined('DB_PASS') or define('DB_PASS', '');
+
