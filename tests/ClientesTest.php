9d980a4c80c325ba42ad072a8af046ae17fcd806
Diego Carvalho <diegocarvalho.dev@gmail.com>
2020-10-15 03:10:50 -0300

    os testes de get passaram

--- tests/ClientesTest.php
+++ tests/ClientesTest.php
@@ -3,23 +3,62 @@ namespace Test;
 
 require_once 'vendor/autoload.php';
 
-use App\Models\Cliente;
-use App\Repositorios\ClienteRepository;
-
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
 class ClientesTest extends TestCase
 {
-	public static $repository; 
-	
-	private static function setRepository()
+
+	private $http;
+
+	public function setUp(): void
 	{
-		$clienteRepository = new ClienteRepository();
-		self::$repository = $clienteRepository;
+		$this->http = new Client(['base_uri' => 'http://clientes.localhost']);
+		//$response = $client->request('GET', '/api/users?page=1');
 	}
+
+    	public function tearDown(): void{
+        	$this->http = null;
+    	}
 
 	public function testClientesGetAll()
 	{
-		$clientes = self::$repository->getAll();
-   		$this->assertTrue(!empty(json_decode($clientes, true)));
-  }
+		$response = $this->http->request('GET', '/clientes');
+		
+		$data = $response->getBody();
+		$this->assertEquals($data, '[{"id":2,"nome":"dieg","email":"dieo","telefone":"89788765544","cpf":"765434589"}]');
+ 	}
+ 	
+	public function testClientesGetOne()
+	{
+		$response = $this->http->request('GET', '/cliente/1');
+		
+		$data = $response->getBody();
+		$this->assertEquals($data, '{"id":1,"nome":"dieg","email":"dieo","telefone":"89788765544","cpf":"765434589"}');
+ 	}
+ 	
+	public function testClientesCreate()
+	{
+		$response = $this->http->request('POST', '/api/users', ['form_params' => [
+		    'name' => 'Dan',
+		    'job' => 'Full Stack Dev'
+		]]);
+		
+		$data = $response->getBody();
+		$this->assertEquals($data, '{"id":1,"nome":"dieg","email":"dieo","telefone":"89788765544","cpf":"765434589"}');
+ 	}
+ 	
+	public function testClientesDelete()
+	{
+		$response = $this->http->request('GET', '/cliente/1');
+		$data = $response->getBody();
+		$this->assertEquals($data, '{"id":1,"nome":"dieg","email":"dieo","telefone":"89788765544","cpf":"765434589"}');
+ 	}
+ 	
+	public function testClientesUpdate()
+	{
+		$response = $this->http->request('GET', '/cliente/1');
+		$data = $response->getBody();
+		$this->assertEquals($data, '{"id":1,"nome":"dieg","email":"dieo","telefone":"89788765544","cpf":"765434589"}');
+ 	}
+}
