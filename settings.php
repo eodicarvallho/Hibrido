e983ee721e2c3c0f089b9cae70c7ab18fd473d16
Diego Carvalho <diegocarvalho.dev@gmail.com>
2020-10-16 10:49:01 -0300

    setting

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('DB_HOST') or define('DB_HOST', '');
+defined('DB_USER') or define('DB_USER', '');
+defined('DB_NAME') or define('DB_NAME', '');
+defined('DB_PASS') or define('DB_PASS', '');
+defined('DB_SQLITE') or define('DB_SQLITE', __dir__ . '/db.sqlite');
+defined('LOG_DIR') or define('LOG_DIR', __dir__ . '/log/');
+
